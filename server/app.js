6431c479e1b540fdadcdb6d46a59a119796f0115
Ryan Gerome Regis <ryangeromeregis@gmail.com>
2021-12-23 11:20:47 +0800

    Added server folder for server part of project, before it was all in the root folder, created a server folder for organizational reasons

--- server/app.js
+++ server/app.js
@@ -1,9 +1,6 @@
 const express = require("express");
+const path = require('path');
 const app = express();
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
 
 app.post("/api", (req, res) => {
     console.log("Connected to React!!!");
@@ -11,5 +8,7 @@ app.post("/api", (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
+const buildPath = path.join(__dirname, 'client', 'dist');
+app.use(express.static(buildPath));
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
