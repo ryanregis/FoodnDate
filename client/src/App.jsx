2fa73309cb4349c97393abf7011cbd30c0ace911
Ronald I. Velarde <ronv833@example.com>
2022-01-05 14:19:21 +0800

    Changes to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider, Typography } from "@mui/material";
 import theme from "./Theme";
-<<<<<<< HEAD
-import AboutUs from "./pages/AboutUs";
-import ContactUs from "./pages/ContactUs";
-=======
 import './App.css';
 
->>>>>>> 4fe4616f15e90acf2edc723d544b3b73ce931c4b
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
@@ -16,15 +11,9 @@ function App() {
   return (
     // <Router>
     <ThemeProvider theme={theme}>
-<<<<<<< HEAD
-      <Header />
-      <AboutUs/>
-      <ContactUs/>
-=======
       {/* <Header /> */}
       <Login />
       {/* <Footer /> */}
->>>>>>> 4fe4616f15e90acf2edc723d544b3b73ce931c4b
       {/* <Typography variant="h2">FoodnDate</Typography> */}
     </ThemeProvider>
     // </Router>
