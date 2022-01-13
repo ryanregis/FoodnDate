c6d58dc2219abe9436b8918699e340e585512609
Ryan Gerome Regis <ryangeromeregis@gmail.com>
2022-01-13 15:09:01 +0800

    Fixed some layout in appointments

--- client/src/pages/Appointment.jsx
+++ client/src/pages/Appointment.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grid, Typography, Box, TextField, Button, Card, CardActionArea, CardContent, Table, TableHead, TableRow, TableCell, TableBody, TableContainer } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import moment from 'moment';
@@ -6,14 +6,16 @@ import DateAdapter from '@mui/lab/AdapterMoment'
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import StaticDatePicker from '@mui/lab/StaticDatePicker';
 import "../App.css";
+import swal from 'sweetalert';
 
 const useStyles = makeStyles(() => ({
     setAppoint: {
         borderRadius: '20px',
         border: '2px solid #6A7EFC',
         display: 'flex',
-        flexDirection: 'row',
         justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
         padding: '2%',
         fontSize: 'clamp(1.3rem, 1vw, 2rem)',
     },
@@ -31,7 +33,7 @@ const useStyles = makeStyles(() => ({
     },
 
 }))
-let storeInfo = localStorage.getItem('memos' ) ? JSON.parse(localStorage.getItem('memos')) : [];
+let storeInfo = localStorage.getItem('memos') ? JSON.parse(localStorage.getItem('memos')) : [];
 const Appointment = () => {
     const [value, setValue] = React.useState(new moment());
     const [clock, setClock] = React.useState(`${moment().format('MMMM Do YYYY, h:mm a')}`);
@@ -43,46 +45,54 @@ const Appointment = () => {
     }, []);
     const [memo, setMemo] = useState('');
     const [id, setID] = useState(Date.now);
-    const [infoEntry, setInfoEntry] =useState(storeInfo);
+    const [infoEntry, setInfoEntry] = useState(storeInfo);
 
     const handleMemo = (e) => {
-        setMemo(e.target.value); 
+        setMemo(e.target.value);
     };
 
     const handleDeleteEach = (e) => {
-
-        if (window.confirm("Do you really want to delete this entry?")) {
-            const numId = parseInt(e.target.value);
-            const eachDelete = [...infoEntry].filter((item) => {
-                return item.id !== numId;
+        swal({
+            title: "Do you really want to delete this entry?",
+            text: "",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        }).then((willDelete) => {
+            if (willDelete) {
+                const numId = parseInt(e.target.value);
+                const eachDelete = [...infoEntry].filter((item) => {
+                    return item.id !== numId;
                 })
-            setInfoEntry(eachDelete);
-        }
-        else {
+                setInfoEntry(eachDelete);
+            }
+            else {
                 return false;
-        }
-    
-        };
+            }
+        })
+
+    };
 
     const handleProductSubmit = (e) => {
         e.preventDefault();
         setID(Date.now);
-        
-        if (memo!=='' ) {
-            const entryInput = {id, memo};
-            alert ("You Have Save Your Memo")
-            setInfoEntry([...infoEntry, entryInput]);      
+
+        if (memo !== '') {
+            const entryInput = { id, memo };
+            alert("You Have Save Your Memo")
+            setInfoEntry([...infoEntry, entryInput]);
         }
         else {
             alert('Please Put an Entry');
         }
-        
-        setMemo('');    
-    } 
 
-    useEffect(()=>{
-        localStorage.setItem('memos', JSON.stringify(infoEntry));},[infoEntry]);
-    
+        setMemo('');
+    }
+
+    useEffect(() => {
+        localStorage.setItem('memos', JSON.stringify(infoEntry));
+    }, [infoEntry]);
+
     return (
         <div>
             <Grid container sx={{ bgcolor: 'white.main' }}>
@@ -96,13 +106,13 @@ const Appointment = () => {
                     <Grid container p={5} >
                         <Grid item sm={12} xs={12}>
                             <Box className={classes.setAppoint}>
-                                Hey @user has someone invited you for a virtual date?
-                                <Box >
-                                    <form>
+                                Hey { } has someone invited you for a virtual date?
+                                <form>
+                                    <Box display="flex" alignItems="center" >
                                         <TextField className={classes.fieldInput} variant='outlined' label='Enter invite code' />
                                         <Button className={classes.fieldInput} variant='contained' sx={{ bgcolor: 'primary.main' }}>Set Appointment</Button>
-                                    </form>
-                                </Box>
+                                    </Box>
+                                </form>
                             </Box>
                         </Grid>
 
@@ -141,31 +151,31 @@ const Appointment = () => {
                                         </Typography>
                                     </CardContent>
                                     <CardContent className={classes.memoDesign}>
-                                        <Box sx={{ display: 'grid', gridTemplateRows: { sm: '1fr 1fr', xs: '1fr 1fr' }, justifyContent:'center'}}>
-                                        <form onSubmit={handleProductSubmit}>
-                                            <TextField id="inputMemo" name="inputMemo"  value={memo} onChange={handleMemo} className={classes.fieldInput} variant='outlined' label='Input Details' />
-                                            <Button type='submit' className={classes.fieldInput} variant='contained' sx={{ bgcolor: 'primary.main', marginLeft:'5px'}}>Save</Button>
-                                        </form>
+                                        <Box sx={{ display: 'grid', gridTemplateRows: { sm: '1fr 1fr', xs: '1fr 1fr' }, justifyContent: 'center' }}>
+                                            <form onSubmit={handleProductSubmit}>
+                                                <TextField id="inputMemo" name="inputMemo" value={memo} onChange={handleMemo} className={classes.fieldInput} variant='outlined' label='Input Details' />
+                                                <Button type='submit' className={classes.fieldInput} variant='contained' sx={{ bgcolor: 'primary.main', marginLeft: '5px' }}>Save</Button>
+                                            </form>
                                         </Box>
-                                            <TableContainer>
-                                                <Table>
-                                                    <TableHead>
-                                                        <TableRow>
-                                                            <TableCell>Memo</TableCell>
-                                                            <TableCell>Action</TableCell>
-                                                        </TableRow>
-                                                    </TableHead>
+                                        {memo && <TableContainer>
+                                            <Table>
+                                                <TableHead>
+                                                    <TableRow>
+                                                        <TableCell>Memo</TableCell>
+                                                        <TableCell>Action</TableCell>
+                                                    </TableRow>
+                                                </TableHead>
 
-                                                    <TableBody >
-                                                        {infoEntry.map((content)=> (
-                                                            <TableRow key={content.id}>
-                                                                <TableCell >{content.memo}</TableCell>
-                                                                <TableCell ><Button value={content.id}  onClick={handleDeleteEach}>Delete</Button></TableCell>
-                                                            </TableRow>
-                                                        ))}
-                                                    </TableBody>
-                                                </Table>
-                                            </TableContainer>
+                                                <TableBody >
+                                                    {infoEntry.map((content) => (
+                                                        <TableRow key={content.id}>
+                                                            <TableCell >{content.memo}</TableCell>
+                                                            <TableCell ><Button value={content.id} onClick={handleDeleteEach}>Delete</Button></TableCell>
+                                                        </TableRow>
+                                                    ))}
+                                                </TableBody>
+                                            </Table>
+                                        </TableContainer>}
                                     </CardContent>
                                 </CardActionArea>
                             </Card>
